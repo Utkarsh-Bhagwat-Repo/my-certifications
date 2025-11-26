669ebb96645c4f92cb7bffb42ec1804f29e90102
Utkarsh-Bhagwat-Repo <utkarsh3178@gmail.com>
2025-11-26 11:42:54 +0530

    Add files via upload

---
+++ HealthTrack360Application.java
@@ -0,0 +1,13 @@
+package com.healthtrack360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrack360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthTrack360Application.class, args);
+        System.out.println("HealthTrack360 Backend v7 is running... Added ");
+    }
+}
